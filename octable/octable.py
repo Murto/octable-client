394ad9aeb896c9186d7a1468459d51784e49a45a
Murray Steele <contact@murto.dev>
2019-07-17 17:00:18 +0100

    Remove layout elements as members of widget

--- octable/octable.py
+++ octable/octable.py
@@ -10,14 +10,14 @@ class OctableWidget(QWidget):
 
   def setLayoutMainMenu(self):
     layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
-    self.welcome = QLabel('Welcome to the Octable client')
-    layout.addWidget(self.welcome, alignment=Qt.AlignCenter)
-    self.createCharacterButton = QPushButton('Create Character')
-    layout.addWidget(self.createCharacterButton, alignment=Qt.AlignCenter)
-    self.manageCharactersButton = QPushButton('Manage Characters')
-    layout.addWidget(self.manageCharactersButton, alignment=Qt.AlignCenter)
-    self.settingsButton = QPushButton('Settings')
-    layout.addWidget(self.settingsButton, alignment=Qt.AlignCenter)
+    welcome = QLabel('Welcome to the Octable client')
+    layout.addWidget(welcome, alignment=Qt.AlignCenter)
+    createCharacterButton = QPushButton('Create Character')
+    layout.addWidget(createCharacterButton, alignment=Qt.AlignCenter)
+    manageCharactersButton = QPushButton('Manage Characters')
+    layout.addWidget(manageCharactersButton, alignment=Qt.AlignCenter)
+    settingsButton = QPushButton('Settings')
+    layout.addWidget(settingsButton, alignment=Qt.AlignCenter)
     self.setLayout(layout)
 
 if __name__ == '__main__':
