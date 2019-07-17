2d24a190d977ebe0993348bbb4ebf1285c670454
Murray Steele <contact@murto.dev>
2019-07-17 16:37:11 +0100

    Make custom octable widget

--- src/octable.py
+++ src/octable.py
@@ -1,10 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QLabel
+import sys
+from PyQt5.QtWidgets import QApplication, QBoxLayout, QLabel, QPushButton, QWidget
+from PyQt5.QtCore import Qt
 
-def main():
-  app = QApplication([])
-  label = QLabel('Welcome to the Octable client')
-  label.show()
-  app.exec_()
+class OctableWidget(QWidget):
+
+  def __init__(self):
+    super().__init__()
+    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
+    self.welcome = QLabel('Welcome to the Octable client')
+    layout.addWidget(self.welcome, alignment=Qt.AlignCenter)
+    self.setLayout(layout)
 
 if __name__ == '__main__':
-  main()
+  app = QApplication(sys.argv)
+  main = OctableWidget()
+  main.show()
+  sys.exit(app.exec_())
