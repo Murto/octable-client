9c4900adddfb70713d9f25816a3d1262aa4fbb20
Murray Steele <contact@murto.dev>
2019-07-22 16:20:01 +0100

    Add character management interface and navigation to stacked widget

--- octable/octable.py
+++ octable/octable.py
@@ -3,6 +3,7 @@ from mainmenu import MainMenu
 from selectgame import SelectGame
 from settings import Settings
 from installgames import InstallGames
+from managecharacters import ManageCharacters
 import sys
 
 def test():
@@ -16,22 +17,29 @@ class OctableWidget(QStackedWidget):
     self.selectgame = SelectGame()
     self.settings = Settings()
     self.installgames = InstallGames()
+    self.managecharacters = ManageCharacters()
     self.addWidget(self.mainmenu)
     self.addWidget(self.selectgame)
     self.addWidget(self.settings)
     self.addWidget(self.installgames)
+    self.addWidget(self.managecharacters)
     self.mainmenu.setCreateCharacterCallback(self.showSelectGame)
+    self.mainmenu.setManageCharactersCallback(self.showManageCharacters)
     self.mainmenu.setSettingsCallback(self.showSettings)
     self.selectgame.setBackCallback(self.showMainMenu)
     self.selectgame.setInstallGamesCallback(self.showInstallGames)
     self.settings.setBackCallback(self.showMainMenu)
     self.installgames.setBackCallback(self.showSelectGame)
+    self.managecharacters.setBackCallback(self.showMainMenu)
   
   def showMainMenu(self):
     self.setCurrentWidget(self.mainmenu)
 
   def showSelectGame(self):
     self.setCurrentWidget(self.selectgame)
+
+  def showManageCharacters(self):
+    self.setCurrentWidget(self.managecharacters)
 
   def showSettings(self):
     self.setCurrentWidget(self.settings)
