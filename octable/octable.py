daa2441e2f035b18c42fbc4d8bbfdfc23294f532
Murray Steele <contact@murto.dev>
2019-07-22 15:50:53 +0100

    Add navigation for InstallGames

--- octable/octable.py
+++ octable/octable.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QApplication, QStackedWidget
 from mainmenu import MainMenu
 from selectgame import SelectGame
 from settings import Settings
+from installgames import InstallGames
 import sys
 
 def test():
@@ -14,13 +15,17 @@ class OctableWidget(QStackedWidget):
     self.mainmenu = MainMenu()
     self.selectgame = SelectGame()
     self.settings = Settings()
+    self.installgames = InstallGames()
     self.addWidget(self.mainmenu)
     self.addWidget(self.selectgame)
     self.addWidget(self.settings)
+    self.addWidget(self.installgames)
     self.mainmenu.setCreateCharacterCallback(self.showSelectGame)
     self.mainmenu.setSettingsCallback(self.showSettings)
     self.selectgame.setBackCallback(self.showMainMenu)
+    self.selectgame.setInstallGamesCallback(self.showInstallGames)
     self.settings.setBackCallback(self.showMainMenu)
+    self.installgames.setBackCallback(self.showSelectGame)
   
   def showMainMenu(self):
     self.setCurrentWidget(self.mainmenu)
@@ -30,6 +35,9 @@ class OctableWidget(QStackedWidget):
 
   def showSettings(self):
     self.setCurrentWidget(self.settings)
+
+  def showInstallGames(self):
+    self.setCurrentWidget(self.installgames)
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
