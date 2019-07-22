73fe95ae9e54cf192d2d8caddb4d381162652230
Murray Steele <contact@murto.dev>
2019-07-22 15:32:09 +0100

    Add basic widget switching between MainMenu and SelectGame

--- octable/octable.py
+++ octable/octable.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QStackedWidget
 from mainmenu import MainMenu
+from selectgame import SelectGame
 import sys
 
 def test():
@@ -9,7 +10,18 @@ class OctableWidget(QStackedWidget):
 
   def __init__(self):
     super().__init__()
-    self.addWidget(MainMenu())
+    self.mainmenu = MainMenu()
+    self.selectgame = SelectGame()
+    self.addWidget(self.mainmenu)
+    self.addWidget(self.selectgame)
+    self.mainmenu.setCreateCharacterCallback(self.showSelectGame)
+    self.selectgame.setBackCallback(self.showMainMenu)
+  
+  def showMainMenu(self):
+    self.setCurrentWidget(self.mainmenu)
+
+  def showSelectGame(self):
+    self.setCurrentWidget(self.selectgame)
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
