f7ee2c05f42c4bb90e319de478766385f0c1eaf0
Murray Steele <contact@murto.dev>
2019-07-22 15:45:22 +0100

    Add navigation between settings interface and mainmenu

--- octable/octable.py
+++ octable/octable.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QStackedWidget
 from mainmenu import MainMenu
 from selectgame import SelectGame
+from settings import Settings
 import sys
 
 def test():
@@ -12,16 +13,23 @@ class OctableWidget(QStackedWidget):
     super().__init__()
     self.mainmenu = MainMenu()
     self.selectgame = SelectGame()
+    self.settings = Settings()
     self.addWidget(self.mainmenu)
     self.addWidget(self.selectgame)
+    self.addWidget(self.settings)
     self.mainmenu.setCreateCharacterCallback(self.showSelectGame)
+    self.mainmenu.setSettingsCallback(self.showSettings)
     self.selectgame.setBackCallback(self.showMainMenu)
+    self.settings.setBackCallback(self.showMainMenu)
   
   def showMainMenu(self):
     self.setCurrentWidget(self.mainmenu)
 
   def showSelectGame(self):
     self.setCurrentWidget(self.selectgame)
+
+  def showSettings(self):
+    self.setCurrentWidget(self.settings)
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
