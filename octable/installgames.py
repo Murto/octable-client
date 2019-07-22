1eed1cb48cb22c5a21a09959d49c6a4052e507f4
Murray Steele <contact@murto.dev>
2019-07-22 15:46:20 +0100

    Add install games interface

---
+++ octable/installgames.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QBoxLayout, QPushButton, QWidget
+from PyQt5.QtCore import Qt
+
+class InstallGames(QWidget):
+
+  def __init__(self):
+    super().__init__()
+    layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
+    self.backButton = QPushButton('Back')
+    layout.addWidget(self.backButton, alignment=Qt.AlignCenter)
+    self.setLayout(layout)
+
+  def setBackCallback(self, callback):
+    self.backButton.clicked.connect(callback)
