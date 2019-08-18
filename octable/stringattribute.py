922a210d470dc530cabc4d653b68c18038dc19d7
Murray Steele <contact@murto.dev>
2019-08-18 10:29:32 +0100

    Add basic string and number attributes

---
+++ octable/stringattribute.py
@@ -0,0 +1,12 @@
+class StringAttribute:
+
+  def __init__(self, string):
+    assert(isinstance(string, str))
+    self._value = string
+
+  def getValue(self):
+    return self._value
+
+  def setValue(self, string):
+    assert(isinstance(string, str))
+    self._value = string
