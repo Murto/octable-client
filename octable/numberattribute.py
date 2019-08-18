f78552ea33ba16c2430a5411a407d05c8ab538e9
Murray Steele <contact@murto.dev>
2019-08-18 10:30:47 +0100

    Exchange c-like or for python or

--- octable/numberattribute.py
+++ octable/numberattribute.py
@@ -1,12 +1,12 @@
 class NumberAttribute:
 
   def __init__(self, number):
-    assert(isinstance(number, int) || isinstance(number, float))
+    assert(isinstance(number, int) or isinstance(number, float))
     self._value = number
 
   def getValue(self):
     return self._value
 
   def setValue(self, number):
-    assert(isinstance(number, int) || isinstance(number, float))
+    assert(isinstance(number, int) or isinstance(number, float))
     self._value = number
