f2c94ac51300261869e0add5223cff765e5fa738
Murray Steele <contact@murto.dev>
2019-08-18 12:31:56 +0100

    Move attributes into a single file

--- octable/attributes.py
+++ octable/attributes.py
@@ -12,3 +12,18 @@ class NumberAttribute:
   def setValue(self, number):
     assert(isinstance(number, int) or isinstance(number, float))
     self._value = number
+
+class StringAttribute:
+
+  TYPE = 'STRING'
+
+  def __init__(self, string):
+    assert(isinstance(string, str))
+    self._value = string
+
+  def getValue(self):
+    return self._value
+
+  def setValue(self, string):
+    assert(isinstance(string, str))
+    self._value = string
